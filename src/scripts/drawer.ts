a7776addb19a1999dc83a94941b7ad53c77017dc
yurika1202 <yrpn120293@gmail.com>
2022-10-12 11:06:33 +0900

    add:背面スクロール制御 #11

--- src/scripts/drawer.ts
+++ src/scripts/drawer.ts
@@ -1,44 +1,46 @@
-import 'typed-query-selector';
-
-const btn = document.getElementById('js_drawerNav_btn') as HTMLButtonElement;
-const nav = document.getElementById('js_drawerNav') as HTMLElement;
-
 /**
  * ドロワーの開閉制御
- * @param btn ドロワー開閉用のボタン
- * @param target 表示させたいメニューコンテンツ要素
  */
-const drawerToggle = (btn: HTMLButtonElement, target: HTMLElement) => {
-  /** メニューコンテンツ表示時の各属性値設定 */
-  const openContents = () => {
-    btn.setAttribute('aria-expanded', 'true');
-    btn.setAttribute('aria-label', 'メニューを閉じる');
-    target.setAttribute('aria-hidden', 'false');
-  };
 
-  /** メニューコンテンツ非表示時の各属性値設定 */
-  const closeContents = () => {
-    btn.setAttribute('aria-expanded', 'false');
-    btn.setAttribute('aria-label', 'メニューを開く');
-    target.setAttribute('aria-hidden', 'true');
-  };
+import { scrollLock } from './logics/scrollLock';
 
-  /** ドロワー開閉ボタンクリック時の挙動 */
-  btn.addEventListener('click', () => {
-    const btnState = btn.getAttribute('aria-expanded');
-    if (btnState === 'false') {
-      openContents();
-    } else {
-      closeContents();
-    }
-  });
+const btn = document.getElementById('js_drawerNav_btn') as HTMLButtonElement;
+const nav = document.getElementById('js_drawerNav') as HTMLDivElement;
 
-  /** ドロワーメニュー外クリック時の挙動 */
-  document.addEventListener('click', e => {
-    if (e.target instanceof HTMLElement && !e.target.closest('#js_drawerNav')) {
-      closeContents();
-    }
-  });
+/** コンテンツ表示時の各属性値設定 */
+const openContents = () => {
+  btn.setAttribute('aria-expanded', 'true');
+  btn.setAttribute('aria-label', 'メニューを閉じる');
+  nav.setAttribute('aria-hidden', 'false');
 };
 
-export default drawerToggle(btn, nav);
+/** コンテンツ非表示時の各属性値設定 */
+const closeContents = () => {
+  btn.setAttribute('aria-expanded', 'false');
+  btn.setAttribute('aria-label', 'メニューを開く');
+  nav.setAttribute('aria-hidden', 'true');
+};
+
+/** ドロワー開閉ボタンクリック時の挙動 */
+btn.addEventListener('click', event => {
+  event.stopPropagation();
+  const isShow = nav.classList.contains('is_show');
+  if (!isShow) {
+    nav.classList.add('is_show');
+    openContents();
+    document.body.classList.add('is_scrollLock');
+    document.addEventListener('touchmove', scrollLock, { passive: false });
+  } else {
+    nav.classList.remove('is_show');
+    closeContents();
+    document.body.classList.remove('is_scrollLock');
+    document.removeEventListener('touchmove', scrollLock);
+  }
+});
+
+/** ドロワーメニュー外クリック時の挙動 */
+document.addEventListener('click', e => {
+  if (e.target instanceof HTMLElement && !e.target.closest('#js_drawerNav')) {
+    closeContents();
+  }
+});
