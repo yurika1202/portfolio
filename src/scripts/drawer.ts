1663467c379c158493940d57ccb0a8274d970274
yurika1202 <yrpn120293@gmail.com>
2022-11-01 03:16:07 +0900

    add:開閉状態に合わせてタイトル変更 #60

--- src/scripts/drawer.ts
+++ src/scripts/drawer.ts
@@ -3,43 +3,42 @@
  */
 
 import { scrollLock } from './logics/scrollLock';
-import {
-  focusElement,
-  focusFirstElement,
-  focusBackContents,
-} from './logics/focusControl';
+import { focusElement, focusFirstElement, focusBackContents } from './logics/focusControl';
 
-const btn = document.getElementById('js_drawerNav_btn') as HTMLButtonElement;
-const nav = document.getElementById('js_drawerNav') as HTMLDivElement;
+const btn = document.getElementById('js_drawerNav_btn') as HTMLButtonElement | null;
+const nav = document.getElementById('js_drawerNav') as HTMLDivElement | null;
+const btnTitle = btn?.querySelector('text');
 
 /** コンテンツ表示処理 */
 const openContents = () => {
-  nav.classList.add('is_show');
+  nav?.classList.add('is_show');
   document.body.classList.add('is_scrollLock');
-  btn.setAttribute('aria-expanded', 'true');
-  btn.setAttribute('aria-label', 'メニューを閉じる');
-  nav.setAttribute('aria-hidden', 'false');
-  focusElement(nav);
+  btn?.setAttribute('aria-expanded', 'true');
+  btn?.setAttribute('aria-label', 'メニューを閉じる');
+  nav?.setAttribute('aria-hidden', 'false');
+  focusElement(nav!);
+  btnTitle!.textContent = 'とじる';
 };
 
 /** コンテンツ非表示処理 */
 const closeContents = () => {
-  nav.classList.remove('is_show');
+  nav?.classList.remove('is_show');
   document.body.classList.remove('is_scrollLock');
-  btn.setAttribute('aria-expanded', 'false');
-  btn.setAttribute('aria-label', 'メニューを開く');
-  nav.setAttribute('aria-hidden', 'true');
-  focusElement(nav);
+  btn?.setAttribute('aria-expanded', 'false');
+  btn?.setAttribute('aria-label', 'メニューを開く');
+  nav?.setAttribute('aria-hidden', 'true');
+  focusElement(nav!);
+  btnTitle!.textContent = 'もくじ';
 };
 
 /** ドロワー開閉ボタンクリック時の挙動 */
-btn.addEventListener('click', event => {
+btn?.addEventListener('click', event => {
   event.stopPropagation();
-  const isShow = nav.classList.contains('is_show');
+  const isShow = nav?.classList.contains('is_show');
   if (!isShow) {
     openContents();
     document.addEventListener('touchmove', scrollLock, { passive: false });
-    focusFirstElement(nav);
+    focusFirstElement(nav!);
   } else {
     closeContents();
     document.removeEventListener('touchmove', scrollLock);
@@ -49,10 +48,7 @@ btn.addEventListener('click', event => {
 
 /** ドロワーメニュー外クリック時の挙動 */
 document.addEventListener('click', event => {
-  if (
-    event.target instanceof HTMLElement &&
-    !event.target.closest('#js_drawerNav')
-  ) {
+  if (event.target instanceof HTMLElement && event.target.closest('#js_drawerNav')) {
     closeContents();
     focusBackContents();
   }
@@ -60,9 +56,9 @@ document.addEventListener('click', event => {
 
 /** Escapeキー選択時の挙動 */
 const footer = document.querySelector('footer');
-footer!.addEventListener('keydown', event => {
+footer?.addEventListener('keydown', event => {
   if (event.code === 'Escape') {
     closeContents();
-    btn.focus();
+    btn?.focus();
   }
 });
