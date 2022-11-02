4f33194d09b92d44c18f05fdd3281bcc9409362a
yurika1202 <yrpn120293@gmail.com>
2022-11-03 04:30:35 +0900

    fix:contactページのドロワー背面フォーカス制御の修正 #64

--- src/scripts/logics/focusControl.ts
+++ src/scripts/logics/focusControl.ts
@@ -31,21 +31,23 @@ export const focusElement = (parentElement: HTMLElement) => {
  */
 export const focusBackContents = () => {
   const header = document.querySelectorAll('header a, header button') as NodeListOf<HTMLElement>;
-  const main = document.querySelectorAll('main a, main button') as NodeListOf<HTMLElement>;
-  const notFocusElements = document.querySelectorAll(
-    '.js_notFocus a, .js_notFocus button'
+  const main = document.querySelectorAll(
+    'main a, main button, main input, main textarea'
   ) as NodeListOf<HTMLElement>;
+  const notFocusElements = document.querySelectorAll('.js_notFocus') as NodeListOf<HTMLElement>;
   const hiddenElements = [...header, ...main, ...notFocusElements];
 
   hiddenElements.forEach(element => {
-    if (element.classList.contains('js_focus_disabled')) {
+    if (element.classList.contains('is_focusDisable')) {
       element.removeAttribute('aria-hidden');
-      element.removeAttribute('tabindex');
-      element.classList.remove('js_focus_disabled');
+      [...notFocusElements].includes(element)
+        ? element.setAttribute('tabindex', '0')
+        : element.removeAttribute('tabindex');
+      element.classList.remove('is_focusDisable');
     } else {
       element.setAttribute('aria-hidden', 'true');
       element.setAttribute('tabindex', '-1');
-      element.classList.add('js_focus_disabled');
+      element.classList.add('is_focusDisable');
     }
   });
 };
