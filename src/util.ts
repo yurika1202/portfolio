5e1abc527f07b8b0c3dd5ed237f6051b42c17bb4
yurika1202 <yrpn120293@gmail.com>
2022-11-09 01:49:27 +0900

    fix:あれこれ整理 #68

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,11 @@
+import type { MarkdownInstance } from 'astro';
+import type { PostType } from '@/types';
+
+/**
+ * サムネイルタイプ別にCSSクラスを作成
+ * @param type サムネイルタイプ
+ * @returns サムネイルCSSクラス名
+ */
 export const thumbnailsClassType = (type: string) => {
   switch (type) {
     case 'icon':
@@ -7,4 +15,16 @@ export const thumbnailsClassType = (type: string) => {
     default:
       break;
   }
+};
+
+/**
+ * 投稿日の新しい順にソート
+ * @param posts ソート対象の配列posts
+ * @returns ソート処理後に上書きした配列posts
+ */
+export const sortedPosts = (posts: MarkdownInstance<PostType>[]) => {
+  return posts.sort(
+    (a, b) =>
+      new Date(b.frontmatter.publishDate).valueOf() - new Date(a.frontmatter.publishDate).valueOf()
+  );
 };
