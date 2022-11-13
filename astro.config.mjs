9747dba89b6ffa7a8240d86af69a0ffd1e10185f
yurika1202 <yrpn120293@gmail.com>
2022-11-14 05:07:29 +0900

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://github.com/yurika1202.io/',
+  site: 'https://yurika1202.github.io',
 });
